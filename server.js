ccaccc0618fe8aca0b2825dc6ec375fdca1e80ad
Sergey Makarov <smakarov@cqg.com>
2018-04-20 20:33:51 +0000

    Fixed port to uppercase

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
